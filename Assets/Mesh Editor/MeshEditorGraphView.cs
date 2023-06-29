c7efdf7f14cfdaf2add1115b4b45e4c6eb2a491b
大森 龍人 <106291438+RyutoOhmori0414@users.noreply.github.com>
2023-06-29 23:33:13 +0900

    [add] FirstCommit

---
+++ Assets/Mesh Editor/MeshEditorGraphView.cs
@@ -0,0 +1,18 @@
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements;
+
+public class MeshEditorGraphView : GraphView
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
