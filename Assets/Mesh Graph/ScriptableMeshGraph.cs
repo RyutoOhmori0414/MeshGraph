98d2b35854342f98c975c07926cbf5f9fd60f1d3
大森 龍人 <106291438+RyutoOhmori0414@users.noreply.github.com>
2023-07-07 20:22:33 +0900

    [fix] スクリプタブるオブジェクトを修正しました

--- Assets/Mesh Graph/ScriptableMeshGraph.cs
+++ Assets/Mesh Graph/ScriptableMeshGraph.cs
@@ -1,9 +1,21 @@
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
-using UnityEditor.Callbacks;
 
 [CreateAssetMenu(fileName = "MeshGraph", menuName = "MeshGraph", order = 0)]
 public class ScriptableMeshGraph : ScriptableObject
 {
-    
+    private List<ProcessingNode> _processedNodes = new List<ProcessingNode>();
+    private ProcessingInputNode _inputNode;
+    private ProcessingOutputNode _outputNode;
+
+    public void InitNode(ProcessingInputNode input, ProcessingOutputNode output)
+    {
+        _inputNode = input;
+        _outputNode = output;
+    }
+
+    public void SetNode(List<ProcessingNode> nodes)
+    {
+        _processedNodes = nodes;
+    }
 }
